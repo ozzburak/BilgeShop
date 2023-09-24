bd52284ab1d2cc26bd57a6df2779311678aa4798
ozzburak <129798261+ozzburak@users.noreply.github.com>
2023-09-24 13:16:26 +0300

    Add files via upload

---
+++ BilgeShop/BilgeShop.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using BilgeShop.Business.Services;
+using BilgeShop.Data.Repository;
+using BilgeShop.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BilgeShop.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+       
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var productDetailDto = _productService.GetProductDetailById(id);
+
+            var viewModel = new ProductDetailViewModel()
+            {
+                ProductId = productDetailDto.ProductId,
+                ProductName = productDetailDto.ProductName,
+                UnitPrice = productDetailDto.UnitPrice,
+                UnitInstock = productDetailDto.UnitInstock,
+                ImagePath = productDetailDto.ImagePath,
+                Description = productDetailDto.Description,
+                CategoryId = productDetailDto.CategoryId,
+                CategoryName = productDetailDto.CategoryName,
+            };
+
+            return View(viewModel);
+        }
+
+
+    }
+}
